de188e661492d067de4dfc382eff43243e19a04a
xuanyu tang <105061290+ukmojb@users.noreply.github.com>
2024-10-13 16:37:24 +0800

    222

--- src/main/java/com/wdcftgg/topalldependents/mods/topaddons/reference/Reference.java
+++ src/main/java/com/wdcftgg/topalldependents/mods/topaddons/reference/Reference.java
@@ -2,8 +2,8 @@ package com.wdcftgg.topalldependents.mods.topaddons.reference;
 
 public class Reference {
 
-    public static final String MOD_ID = "topaddons";
-    public static final String MOD_NAME = "TOP Addons";
+    public static final String MOD_ID = "topalldependents";
+    public static final String MOD_NAME = "TopAllDependents";
     public static final String VERSION = "@VERSION@";
 
     public static final String GUI_FACTORY = "com.wdcftgg.topalldependents.mods.topaddons.config.TOPAddonsGuiFactory";
